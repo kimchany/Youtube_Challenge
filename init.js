84bd4444b716e2827bb616b43e10f624fe3e7ec1
Chany <cykim1991@naver.com>
2020-06-30 00:59:25 +0900

    6/30

--- init.js
+++ init.js
@@ -1,8 +1,13 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Comment";
+import "./models/Video";
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 const handleListening = () =>
-  console.log(`✅ Listening on: http://localhost:${PORT}`);
+  console.log(`✅  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
