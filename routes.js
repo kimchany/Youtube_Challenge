6a15fff7daa1182d679f7ea87458cc0e559b2b42
Chany <cykim1991@naver.com>
2020-07-06 00:57:38 +0900

    7/6

--- routes.js
+++ routes.js
@@ -28,9 +28,8 @@ const routes = {
   userDetail: (id) => {
     if (id) {
       return `/users/${id}`;
-    } else {
-      return USER_DETAIL;
     }
+    return USER_DETAIL;
   },
   editProfile: EDIT_PROFILE,
   changePassword: CHANGE_PASSWORD,
@@ -39,12 +38,21 @@ const routes = {
   videoDetail: (id) => {
     if (id) {
       return `/videos/${id}`;
-    } else {
-      return VIDEO_DETAIL;
     }
+    return VIDEO_DETAIL;
   },
-  editVideo: EDIT_VIDEO,
-  deleteVideo: DELETE_VIDEO,
+  editVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    }
+    return EDIT_VIDEO;
+  },
+  deleteVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    }
+    return DELETE_VIDEO;
+  },
 };
 
 export default routes;
